483bb8673b0976eaf1a06a8f7efc8db8d1eab776
Nils Werner <nils.werner@gmail.com>
2011-07-07 15:24:51 +0200

    add PalmService in main.js

--- application/main.js
+++ application/main.js
@@ -2,6 +2,13 @@ enyo.kind({
    name: "main",
    kind: enyo.Control,
    components: [
-      {content: "Hello world"}
+      {content: "Hello world"},
+      {
+         name: "oauth",
+         kind: "enyo.PalmService",
+         service: "palm://com.ddluk.oauthtest.oauth/",
+         subscribe: true,
+         timeout: 10000
+      }
    ]
 });
