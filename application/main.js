d61138cfc8007f9a901171e2475507bce49598cf
Nils Werner <nils.werner@gmail.com>
2011-07-07 15:22:28 +0200

    Fixed main.js

--- application/main.js
+++ application/main.js
@@ -2,11 +2,6 @@ enyo.kind({
    name: "main",
    kind: enyo.Control,
    components: [
-   {content: "Hello world"},
-   { "library":	{ "name": "foundations", "version": "1.0" } },
-   { "source":	"modules.js" },
-   
+      {content: "Hello world"}
    ]
-   })
-   
-   
+});
